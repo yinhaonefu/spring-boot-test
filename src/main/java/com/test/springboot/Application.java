4d38e385f4642062e363d5920ebad3f041eff1f4
yinhao-mac <yinhao@jd.com>
2018-04-02 21:24:57 +0800

    改为web启动方式

--- src/main/java/com/test/springboot/Application.java
+++ src/main/java/com/test/springboot/Application.java
@@ -2,13 +2,20 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by yinhao on 18/3/27.
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
